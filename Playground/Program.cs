f92c78a7fd14d38a2f2b817855ee0885a23ec11e
Ben <benscr.email@gmail.com>
2025-10-27 17:45:05 +0100

    Update

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,6 +6,7 @@ public static class Program
         PasswordUtility.Initialize();
         Password pwd = new Password();
         Console.WriteLine($"Generated Password \"{pwd.Next()}\"");
+        Console.WriteLine(PasswordUtility.ClassifyPassword(pwd.Next()));
         Password pwd2 = new Password();
     }
 }
